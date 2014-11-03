dc6387319294a716b97d5d969c4e98a7a7097665
David Medvdev <daveymedvedev@gmail.com>
2014-11-03 10:25:18 -0500

    labs
    labs

---
+++ lab4/lab4.sql
@@ -0,0 +1,32 @@
+﻿--SELECT City
+--FROM Agents
+--WHERE AID in
+--	(SELECT AID
+--	FROM ORDERS
+--	WHERE CID = 'c001'
+--	)
+
+--SELECT PID
+--FROM Products
+--WHERE PID in
+--	(SELECT PID
+--	FROM Orders
+--	WHERE AID = 'a03')
+
+--SELECT CID, NAME
+--FROM Customers
+--WHERE cid in
+--	(SELECT cid
+--	FROM Orders
+--	WHERE AID != 'ao4'
+--	)
+
+--SELECT CID, Name
+--FROM Customers
+--WHERE CID in
+--	(SELECT CID
+--	FROM Orders
+--	WHERE PID = 'p01'
+--	or PID = 'p07'
+--	)
+
